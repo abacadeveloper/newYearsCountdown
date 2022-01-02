4929a1f08cbcbeeaa156dc5495957c86bbc461cc
Alberto Baca <60315845+abacadeveloper@users.noreply.github.com>
2022-01-02 16:06:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const hoursEl = document.getElementById('hours');
 const minsEl = document.getElementById('minutes');
 const secondsEl = document.getElementById('seconds');
 
-const newYears = '1 January 2022';
+const newYears = '1 January 2023';
 
 
 function countdown() {
